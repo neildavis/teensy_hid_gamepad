d39c40ff44f35211046d5cd88b5c0617ed8c3ae0
Neil Davis <neil.a.davis@gmail.com>
2022-03-30 14:39:33 +0100

    Add an additional analog input for stick axis z Enable stick button at expense of one keypad button for now.

--- code.py
+++ code.py
@@ -15,8 +15,8 @@ import adafruit_matrixkeypad
 from hid_gamepad import Gamepad
 
 # Classic 4x4 matrix keypad
-cols = [digitalio.DigitalInOut(x) for x in (board.D0, board.D1, board.D2, board.D3)]
-rows = [digitalio.DigitalInOut(x) for x in (board.D4, board.D5, board.D6, board.D7)]
+rows = [digitalio.DigitalInOut(x) for x in (board.D0, board.D1, board.D2, board.D3)]
+cols = [digitalio.DigitalInOut(x) for x in (board.D4, board.D5, board.D6, board.D7)]
 keypad_gp_nums = ((1,2,3,13),
                 (4,5,6,14),
                 (7,8,9,15),
@@ -27,14 +27,16 @@ kp = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keypad_gp_nums)
 gp = Gamepad(usb_hid.devices)
 
 # Create a button. 
-button_pin = digitalio.DigitalInOut(board.D8)
+button_pin = digitalio.DigitalInOut(board.D21)
 button_pin.direction = digitalio.Direction.INPUT
 button_pin.pull = digitalio.Pull.UP
-button_gp_num = 17
+button_gp_num = 1 # dups' one of the 4x4 keypad buttons
 
-# Connect an analog two-axis joystick to A4 and A5.
-ax = analogio.AnalogIn(board.A4)
-ay = analogio.AnalogIn(board.A5)
+# Connect an analog two-axis joystick to A8 and A9.
+ax = analogio.AnalogIn(board.A9)
+ay = analogio.AnalogIn(board.A8)
+# Connect an analog single-axis 'throttle' to A6
+az = analogio.AnalogIn(board.A6)
 
 # Equivalent of Arduino's map() function.
 def range_map(x, in_min, in_max, out_min, out_max):
@@ -44,20 +46,32 @@ def range_map(x, in_min, in_max, out_min, out_max):
 while True:
     # Keypad
     kp_pressed = kp.pressed_keys
-    kp_released = set(list(range(1,button_gp_num))).difference(kp_pressed)
+    kp_released = set(range(1,17)).difference(kp_pressed)
+    # Ignore the keypad button that dups the joystick button
+    try:
+        kp_pressed.remove(button_gp_num)
+        kp_released.remove(button_gp_num)
+    except:
+        pass
+    # report keypad buttons
     gp.press_buttons(*kp_pressed)
     gp.release_buttons(*kp_released)
  
     # Buttons are grounded when pressed (.value = False).
-    # if button_pin.value:
-    #     gp.release_buttons(button_gp_num)
-    # else:
-    #     gp.press_buttons(button_gp_num)
+    if button_pin.value:
+        gp.release_buttons(button_gp_num)
+    else:
+        gp.press_buttons(button_gp_num)
+    # Since stick button dup's a keypad button we don't have to release it
 
     # Analog Stick. Convert range[0, 65535] to -127 to 127
+    xm = range_map(ax.value, 0, 65535, -127, 127)
+    ym = range_map(ay.value, 0, 65535, -127, 127)
+    zm = range_map(az.value, 0, 65535, -127, 127)
     gp.move_joysticks(
-        x=range_map(ax.value, 0, 65535, -127, 127),
-        y=range_map(ay.value, 0, 65535, -127, 127),
+        x=xm,
+        y=ym,
+        z=zm,
     )
-    print(" x", ax.value, "y", ay.value)
+    print(" x:", xm, "y:", ym, "z:", zm, "kpp:", kp_pressed, "kpr:", kp_released)
     
