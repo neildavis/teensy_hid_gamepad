91c296307ba496fb485b03480b713086ec6bdff3
Neil Davis <neildavis@users.noreply.github.com>
2023-07-12 20:36:11 +0100

    Add emulationstation configure command 'conf_es'

--- code.py
+++ code.py
@@ -43,14 +43,24 @@ analog_ins = {
 }
 
 # Enumerate all our digital io inputs as HID button IDs (1-16)
-BUTTON_START        = 10
-BUTTON_SELECT       = 9
-BUTTON_SOUTH_B      = 2
 BUTTON_WEST_Y       = 1
+BUTTON_SOUTH_B      = 2
 BUTTON_EAST_A       = 3
 BUTTON_NORTH_X      = 4
+BUTTON_SHOULDER_L   = 5
+BUTTON_SHOULDER_R   = 6
+BUTTON_TRIGGER_L    = 7
+BUTTON_TRIGGER_R    = 8
+BUTTON_SELECT       = 9
+BUTTON_START        = 10
+BUTTON_THUMB_L      = 11
+BUTTON_THUMB_R      = 12
+BUTTON_HAT_UP       = 13
+BUTTON_HAT_DOWN     = 14
+BUTTON_HAT_LEFT     = 15
+BUTTON_HAT_RIGHT    = 16
 # TODO: Add a further digital io buttons with unique HID button ids in range 1-16 if required
-BUTTON_MAX          = 16
+BUTTON_MAX          = BUTTON_HAT_RIGHT
 
 # CC Volume handled by buttons outside gamepad button range
 BUTTON_VOL_UP       = 20
@@ -91,64 +101,83 @@ def range_map(x, in_min, in_max, out_min, out_max):
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
-def read_cmd_from_serial():
+def process_commands(**cmds):
     global pressed_buttons
+    pressed_buttons.clear()
+    gamepad_axes_values = {'x':0, 'y':0, 'z':0, 'r_z':0}
+    pre_wait = 0.0
+    post_wait = 0.0
+    hold_time = 0.5
+    cc_vol = 0
+    for input, value in cmds.items():
+        try:
+            value = int(value)
+        except:
+            continue
+        if input[0] == 'b':
+            # This is a digital button input value, i.e. b1/b2/ ... /b15/b16
+            if int(value) > 0:
+                pressed_buttons.add(int(input[1:]))
+        elif input in ['x', 'y', 'z', 'r_z']:
+            # This is an analog input axis value, i.e. x/y/z/r_x
+            gamepad_axes_values[input] = value
+        elif input == 'v':
+            # This is a volume value, +ve or -ve
+            cc_vol = value
+        elif input == 'hold':
+            # Hold control(s) for a period of time
+            hold_time = value
+        elif input == 'pre':
+            # Hold control(s) for a period of time
+            pre_wait = value
+        elif input == 'post':
+            # Hold control(s) for a period of time
+            post_wait = value
+    released_buttons = set(range(1,17)).difference(pressed_buttons)
+
+    # pre-wait period
+    sleep(pre_wait)
+    # debug
+    print(f'pressed_buttons={pressed_buttons} : released_buttons={released_buttons}')
+    print(f'gamepad_axes_values={gamepad_axes_values}')
+    print(f'cc_vol={cc_vol}')
+    # update gamepad button values
+    gp.press_buttons(*pressed_buttons)
+    gp.release_buttons(*released_buttons)
+    # update gamepad axes
+    gp.move_joysticks(**gamepad_axes_values) 
+    # update CC volume
+    if cc_vol > 0:
+        cc.press(ConsumerControlCode.VOLUME_INCREMENT)
+    elif cc_vol < 0:
+        cc.press(ConsumerControlCode.VOLUME_DECREMENT)
+    # hold before releasing all buttons and centring axes
+    sleep(hold_time)
+    pressed_buttons.clear()
+    gp.release_all_buttons()
+    gp.move_joysticks(0,0,0,0)
+    cc.release()
+    # post-wait period
+    sleep(post_wait)
+    
+
+def read_cmd_from_serial():
     num_bytes_to_read =  usb_cdc.data.in_waiting
     if num_bytes_to_read > 0:
         cdc_data = usb_cdc.data.readline()
         cdc_str = cdc_data.decode("utf-8")[0:-1]
         print(f'Read {len(cdc_data)} bytes from usb_cdc.data: {cdc_str}')
-        #e.g cdc_str = "B1=0;B2=1; ... ;x=32767;y=-32767;z=0;r_z="
+        if cdc_str == 'conf_es':
+            configure_emulation_station()
+            return True
+        # decode 'name=value' pair commands. e.g cdc_str = "b1=0;b2=1; ... ;x=32767;y=-32767;z=0;r_z="
         try:
             cmds = dict(item.split("=") for item in cdc_str.split(";"))
         except:
             return False
         if len(cmds) > 0:  
             print(f'Decoded cmds: {cmds}')
-            pressed_buttons.clear()
-            gamepad_axes_values = {'x':0, 'y':0, 'z':0, 'r_z':0}
-            wait_time=0.5
-            cc_vol = 0
-            for input, value in cmds.items():
-                try:
-                    value = int(value)
-                except:
-                    continue
-                if input[0] == 'b':
-                    # This is a digital button input value, i.e. b1/b2/ ... /b15/b16
-                    if int(value) > 0:
-                        pressed_buttons.add(int(input[1:]))
-                elif input in ['x', 'y', 'z', 'r_z']:
-                    # This is an analog input axis value, i.e. x/y/z/r_x
-                    gamepad_axes_values[input] = value
-                elif input == 'v':
-                    # This is a volume value, +ve or -ve
-                    if value > 0:
-                        cc.press(ConsumerControlCode.VOLUME_INCREMENT)
-                        cc_vol = 1
-                    elif value < 0:
-                        cc.press(ConsumerControlCode.VOLUME_DECREMENT)
-                        cc_vol = -1
-                elif input == 'w':
-                    # Wait for a period of time
-                    wait_time=value
-            released_buttons = set(range(1,17)).difference(pressed_buttons)
-            # debug
-            print(f'pressed_buttons={pressed_buttons} : released_buttons={released_buttons}')
-            print(f'gamepad_axes_values={gamepad_axes_values}')
-            print(f'cc_vol={cc_vol}')
-            # Update gamepad button values
-            gp.press_buttons(*pressed_buttons)
-            gp.release_buttons(*released_buttons)
-            # update gamepad axes
-            if len(gamepad_axes_values) > 0:
-                gp.move_joysticks(**gamepad_axes_values) 
-            # hold before releasing all buttons and centring axes
-            sleep(wait_time)
-            pressed_buttons.clear()
-            gp.release_all_buttons()
-            gp.move_joysticks(0,0,0,0)
-            cc.release
+            process_commands(**cmds)
             return True
     return False
 
@@ -214,6 +243,42 @@ def update_gamepad_buttons_from_digital_inputs():
     released_buttons = set(range(1,17)).difference(pressed_buttons)
     gp.press_buttons(*pressed_buttons)
     gp.release_buttons(*released_buttons)
+
+def configure_emulation_station():
+    '''
+    Send the necessary commands to automate an Input Configuration in EmulationStation
+    '''
+    cmds = [
+        { f'b{BUTTON_SELECT}'       :1, 'hold'  : 3, 'post' : 1 },   # Initial 'hold any button'
+        { f'b{BUTTON_HAT_UP}'       : 1, 'post' : 1 },
+        { f'b{BUTTON_HAT_DOWN}'     : 1, 'post' : 1 },
+        { f'b{BUTTON_HAT_LEFT}'     : 1, 'post' : 1 },
+        { f'b{BUTTON_HAT_RIGHT}'    : 1, 'post' : 1 },
+        { f'b{BUTTON_START}'        : 1, 'post' : 1 },
+        { f'b{BUTTON_SELECT}'       : 1, 'post' : 1 },
+        { f'b{BUTTON_EAST_A}'       : 1, 'post' : 1 },
+        { f'b{BUTTON_SOUTH_B}'      : 1, 'post' : 1 },
+        { f'b{BUTTON_NORTH_X}'      : 1, 'post' : 1 },
+        { f'b{BUTTON_WEST_Y}'       : 1, 'post' : 1 },
+        { f'b{BUTTON_SHOULDER_L}'   : 1, 'post' : 1 },
+        { f'b{BUTTON_SHOULDER_R}'   : 1, 'post' : 1 },
+        { f'b{BUTTON_TRIGGER_L}'    : 1, 'post' : 1 },
+        { f'b{BUTTON_TRIGGER_R}'    : 1, 'post' : 1 },
+        { f'b{BUTTON_THUMB_L}'      : 1, 'post' : 1 },
+        { f'b{BUTTON_THUMB_R}'      : 1, 'post' : 1 },
+        { 'y'                       : -32767, 'post' : 1 },
+        { 'y'                       : 32767, 'post' : 1 },
+        { 'x'                       : -32767, 'post' : 1 },
+        { 'x'                       : 32767, 'post' : 1 },
+        { 'r_z'                     : -32767, 'post' : 1 },
+        { 'r_z'                     : 32767, 'post' : 1 },
+        { 'z'                       : -32767, 'post' : 1 },
+        { 'z'                       : 32767, 'post' : 1 },
+        { f'b{BUTTON_SELECT}'       : 1, 'post' : 1 },              # Hotkey
+        { f'b{BUTTON_EAST_A}'       : 1 },  # 'OK' / Finish
+      ]
+    for cmd in cmds:
+        process_commands(**cmd)
 
 
 while True:
